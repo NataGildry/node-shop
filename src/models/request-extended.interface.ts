26cfc8b9291f7c2c53b18c13001d98496560ae3d
NataGildry <natalia.podpryatova@gmail.com>
2020-06-30 11:37:31 +0300

    add cart features

--- src/models/request-extended.interface.ts
+++ src/models/request-extended.interface.ts
@@ -1,9 +1,9 @@
 import {Request} from 'express';
 
 import {IUser} from './user.interface';
-//import {IProduct} from './product.interface';
+import {IProduct} from './product.interface';
 
 export interface IRequestExtended extends Request{
     user?: IUser
-  //  product?: IProduct
+    product?: IProduct
 }
